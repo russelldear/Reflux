46f543ba32a68d5885894b962ea08746e43fbe6b
Russell Dear <russell.dear@gmail.com>
2017-05-13 04:35:19 +0000

    Add DD, remove known words

--- Reflux/RefluxController.cs
+++ Reflux/RefluxController.cs
@@ -4,6 +4,7 @@ using Reflux.Services;
 using System.Configuration;
 using System.Linq;
 using System.Collections.Generic;
+using StatsdClient;
 
 namespace Reflux
 {
@@ -11,66 +12,66 @@ namespace Reflux
     {
         public static void Go()
         {
+            RefluxStatsdConfig.Configure();
+
             var logger = NLog.LogManager.GetCurrentClassLogger();
-            
-            try
+
+            using (DogStatsd.StartTimer("RunRemindMe"))
             {
-                var apiKey = ConfigurationManager.AppSettings["FlowdockAPIKey"];
-                var knownCommands = ConfigurationManager.AppSettings["KnownCommands"].Split('|');
-
-                var searchService = new SearchService(apiKey);
-                var reminderService = new ReminderService(apiKey);
-                var tagService = new TagService(apiKey);
-
-                var searchResults = searchService.Search(Constants.RemindMe);
-
-                searchResults = searchResults.Where(r => !r.Tags.Contains(Constants.RemindMeSent)).ToList();
-
-                logger.Info(searchResults.Count + " search results returned from " + searchResults.OrderBy(r => r.CreatedAt).FirstOrDefault()?.CreatedAtString + " onward.");
-
-                List<Exception> errors = new List<Exception>();
-
-                foreach (var searchResult in searchResults)
+                try
                 {
-                    try
+                    var apiKey = ConfigurationManager.AppSettings["FlowdockAPIKey"];
+
+                    var searchService = new SearchService(apiKey);
+                    var reminderService = new ReminderService(apiKey);
+                    var tagService = new TagService(apiKey);
+
+                    var searchResults = searchService.Search(Constants.RemindMe);
+
+                    searchResults = searchResults.Where(r => !r.Tags.Contains(Constants.RemindMeSent)).ToList();
+
+                    DogStatsd.Gauge("RemindMe.SearchResults", searchResults.Count);
+                    logger.Info(searchResults.Count + " search results returned from " + searchResults.OrderBy(r => r.CreatedAt).FirstOrDefault()?.CreatedAtString + " onward.");
+
+                    List<Exception> errors = new List<Exception>();
+
+                    foreach (var searchResult in searchResults)
                     {
-                        var reminderDateTime = Parse.Time(searchResult.Content, searchResult.CreatedAt);
-
-                        if (reminderDateTime.HasValue)
+                        try
                         {
-                            tagService.AddTag(searchResult.Id, searchResult.OriginalFlowName, Constants.RemindMeWillSend);
+                            var reminderDateTime = Parse.Time(searchResult.Content, searchResult.CreatedAt);
 
-                            if (reminderDateTime.Value < DateTime.UtcNow)
+                            if (reminderDateTime.HasValue)
                             {
-                                var reminder = searchResult.ToReminder(reminderDateTime.Value);
+                                tagService.AddTag(searchResult.Id, searchResult.OriginalFlowName, Constants.RemindMeWillSend);
 
-                                reminderService.SendReminder(reminder);
+                                if (reminderDateTime.Value < DateTime.UtcNow)
+                                {
+                                    var reminder = searchResult.ToReminder(reminderDateTime.Value);
+
+                                    reminderService.SendReminder(reminder);
+                                }
                             }
                         }
-                        else
+                        catch (Exception ex)
                         {
-                            if (!knownCommands.Any(c => searchResult.Content.Contains(c)))
-                            {
-                                tagService.AddTag(searchResult.Id, searchResult.OriginalFlowName, Constants.RemindMeUnknown);
-                            }
+                            // Don't break for everyone if one reminder doesn't work.
+                            errors.Add(ex);
                         }
                     }
-                    catch(Exception ex)
+
+                    DogStatsd.Gauge("RemindMe.Errors", errors.Count);
+
+                    if (errors.Count > 0)
                     {
-                        // Don't break for everyone if one reminder doesn't work.
-                        errors.Add(ex);
+                        var errorText = string.Join(Environment.NewLine, errors.Select(e => e.Message).ToList());
+                        throw new Exception("The following errors were encountered while reminding: " + errorText);
                     }
                 }
-
-                if (errors.Count > 0)
+                catch (Exception ex)
                 {
-                    var errorText = string.Join(Environment.NewLine, errors.Select(e => e.Message).ToList());
-                    throw new Exception("The following errors were encountered while reminding: " + errorText);
+                    logger.Error(ex.Message + ex.StackTrace);
                 }
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex.Message + ex.StackTrace);
             }
         }
     }
---
+++ Reflux/RefluxStatsdConfig.cs
@@ -0,0 +1,18 @@
+﻿using StatsdClient;
+
+namespace Reflux
+{
+    public static class RefluxStatsdConfig
+    {
+        public static void Configure()
+        {
+            var metricsConfig = new StatsdConfig()
+            {                
+                StatsdServerName = "127.0.0.1",
+                Prefix = "Reflux" 
+            };
+
+            DogStatsd.Configure(metricsConfig);
+        }
+    }
+}
